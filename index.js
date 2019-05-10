2cce76c366dfdc2ab834512d7011b77e432acb4f
Angelica Costatini <angelicacostatini@DWMAC-FVFV27QAJ1WL.local>
2019-05-10 11:08:22 +0200

    adding style as its own component, adding list component

--- index.js
+++ index.js
@@ -4,51 +4,81 @@
 
 window.addEventListener('DOMContentLoaded', main)
 
-function createHeader (mainElement) {
-  console.log(mainElement)
-  let vdom = {
-    'header': {
-      'h1': 'New Title',
-      'h3': 'Subtitle'
-    },
-    'style': `
-      .header {
-        background: lightpink;
-        padding: 20px;
-      }
-      .h1 {
-        text-align: center;
-      }
-    `
-  }
-  let vdomKeys = Object.keys(vdom)
+function render (rootEl, model) {
 
-  vdomKeys.forEach(key => {
+  let modelKeys = Object.keys(model)
+
+  modelKeys.forEach(key => {
     let el = document.createElement(key)
 
-    if (typeof vdom[key] === 'string') {
-      el.innerText = vdom[key]
+    if (typeof model[key] === 'string') {
+      el.innerHTML = model[key]
     } else {
 
-      let children = Object.keys(vdom[key])
+      let children = Object.keys(model[key])
 
       children.forEach(childKey => {
         let childrenEl = document.createElement(childKey)
-        childrenEl.innerText = vdom[key][childKey]
+        childrenEl.innerHTML = model[key][childKey]
         el.appendChild(childrenEl)
       })
     }
 
-    console.log(el)
+    rootEl.appendChild(el);
   })
 
 }
 
-function organise() {
-
-}
 
 function main () {
+  // Component models ------>
+  let header = {
+    'header': {
+      'h1': 'New Title',
+      'h3': 'Subtitle',
+    }
+  }
+
+  let list = {
+    'ul' : ''
+  }
+
+  let listItem = {
+    'li' : {
+      'h5': 'List title',
+      'p': 'list body'
+    }
+  }
+
+  let  style = {
+    'style': `
+    header {
+      background: lightpink;
+      padding: 20px;
+    }
+    h1 {
+      text-align: center;
+      margin: 0;
+    }
+    h3 {
+      margin: 0;
+    }
+    `
+  }
+  // END ------>
+
   let mainElement = document.querySelector('#app');
-  createHeader(mainElement)
+  let bodyElement = mainElement.closest('body');
+
+  render(mainElement, header)
+  render (mainElement, list)
+
+  let listElement = document.querySelector('ul');
+
+  render (listElement, listItem)
+
+  for (var i = 0; i < 50; i++) {
+    render(listElement, listItem)
+  }
+  render (bodyElement, style)
 }
